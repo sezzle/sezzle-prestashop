594ac131fe539252ef9c6ef2aa2042b1a7856e71
mkreusch <info@kreusch-creative.de>
2021-10-25 23:17:51 +0200

    add checkout widget

--- sezzle.php
+++ sezzle.php
@@ -462,6 +462,7 @@ class Sezzle extends PaymentModule
         }
         $option = new PaymentOption();
         $option->setAction($this->context->link->getModuleLink($this->name, 'redirect', array(), true))
+            ->setAdditionalInformation('<div id="sezzle-checkout-widget"><div id="sezzle-installment-widget-box"></div></div><script src="'.__PS_BASE_URI__ . 'modules/sezzle/views/js/checkout-sezzle.js" type="text/javascript"></script>')
             ->setLogo($this->logo_url);
 
         return [
