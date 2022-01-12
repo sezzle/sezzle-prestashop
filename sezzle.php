e129867adba3f4b1f6e41acdc3c556d8db27f89c
Arijit De <arijit.de@sezzle.com>
2022-01-12 18:53:49 +0530

    PLAT-1787:Vrsion update

--- sezzle.php
+++ sezzle.php
@@ -83,7 +83,7 @@ class Sezzle extends PaymentModule
     {
         $this->name = 'sezzle';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.10';
+        $this->version = '2.0.0';
         $this->author = 'Sezzle';
         $this->module_key = 'de1effcde804e599e716e0eefcb6638c';
         $this->need_instance = 1;
