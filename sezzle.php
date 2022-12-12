dc196836ad3e20e32409bb5767352f2979231eb6
Arijit De <arijit.de@sezzle.com>
2022-12-12 20:37:03 +0530

    MINT-604: Bump version

--- sezzle.php
+++ sezzle.php
@@ -87,7 +87,7 @@ class Sezzle extends PaymentModule
     {
         $this->name = 'sezzle';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.2';
+        $this->version = '2.0.3';
         $this->author = 'Sezzle';
         $this->module_key = 'de1effcde804e599e716e0eefcb6638c';
         $this->need_instance = 1;
