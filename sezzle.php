798049c881b721ce28dd878725140f687015cbaa
Arijit De <arijit.de@sezzle.com>
2024-06-20 12:36:33 -0500

    PLAT-2831: Initialize with config value merchant uuid

--- sezzle.php
+++ sezzle.php
@@ -110,6 +110,7 @@ class Sezzle extends PaymentModule
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall Sezzle?');
         $this->limited_countries = array('US', 'CA', 'DE', 'FR', 'ES', 'IT');
         $this->ps_versions_compliancy = array('min' => '1.7.0.0', 'max' => _PS_VERSION_);
+        $this->merchantUUID = Configuration::get(static::$formFields['merchant_id']);
     }
 
     /**
