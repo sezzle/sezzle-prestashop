7906470f6c1a5d60dc7d516ffe2afa4e05a64fcf
allyson.hammerlund <allyson.hammerlund@sezzle.com>
2026-04-08 16:20:33 -0500

    bump sezzle plugin version for presta 9.1 compatible release

--- sezzle.php
+++ sezzle.php
@@ -85,7 +85,7 @@ class Sezzle extends PaymentModule
     {
         $this->name = 'sezzle';
         $this->tab = 'payments_gateways';
-        $this->version = '3.0.0';
+        $this->version = '3.0.1';
         $this->author = 'Sezzle';
         $this->module_key = 'de1effcde804e599e716e0eefcb6638c';
         $this->need_instance = 1;
